3798e5cc18cc592391f8dbf0eabc7115473eb1b5
sanatonek <sana.tonekaboni@mail.utoronto.ca>
2020-03-31 18:24:11 -0400

    Resolved overlap

--- data/simulated_data.py
+++ data/simulated_data.py
@@ -7,32 +7,39 @@ import pickle
 
 n_signals = 5
 n_states = 4
-transition_matrix = np.eye(n_states)*0.9
+transition_matrix = np.eye(n_states)*0.8
 transition_matrix[0,1] = transition_matrix[1,0] = 0.1
-# transition_matrix[0,2] = transition_matrix[2,0] = 0.1
-# transition_matrix[0,3] = transition_matrix[3,0] = 0.1
-# transition_matrix[0,4] = transition_matrix[4,0] = 0.1
+transition_matrix[0,2] = transition_matrix[2,0] = 0.05
+transition_matrix[0,3] = transition_matrix[3,0] = 0.05
 transition_matrix[2,3] = transition_matrix[3,2] = 0.1
+transition_matrix[2,1] = transition_matrix[1,2] = 0.05
+transition_matrix[3,1] = transition_matrix[1,3] = 0.05
 
 
-def main(n_samples, n_features, sig_len):
+def main(n_samples, sig_len):
     all_signals = []
     all_states = []
     for _ in range(n_samples):
-        sample_signal, sample_state = create_signal(n_features, sig_len)
+        sample_signal, sample_state = create_signal(sig_len)
         all_signals.append(sample_signal)
         all_states.append(sample_state)
-    print(np.array(all_signals).shape)
 
     dataset = np.array(all_signals)
     states = np.array(all_states)
     n_train = int(len(dataset) * 0.8)
     train_data = dataset[:n_train]
     test_data = dataset[n_train:]
-    train_data_n, test_data_n = normalize(train_data[:, np.newaxis, :], test_data[:, np.newaxis, :])
+    train_data_n, test_data_n = normalize(train_data, test_data)
     train_state = states[:n_train]
     test_state = states[n_train:]
-    # plt.plot(train_data_n[0,0,:])
+
+    print("Dataset Shape ====> \tTrainset: ", train_data_n.shape, "\tTestset: ", test_data_n.shape)
+    # f, axes = plt.subplots(3,1)
+    # f.set_figheight(3)
+    # f.set_figwidth(10)
+    # print(train_state[0])
+    # for i,ax in enumerate(axes):
+    #     ax.plot(train_data_n[0,i,:])
     # plt.show()
 
     # Save signals to file
@@ -48,19 +55,30 @@ def main(n_samples, n_features, sig_len):
         pickle.dump(test_state, f)
 
 
-def create_signal(n_features, sig_len, window_size=20):
+def create_signal(sig_len, window_size=50):
     states = []
-    signals = []
+    sig_1 = []
+    sig_2 = []
+    sig_3 = []
     pi = np.ones((1,n_states))/n_states
 
     for _ in range(sig_len//window_size):
         current_state = np.random.choice(n_states, 1, p=pi.reshape(-1))
-        current_signal = ts_generator(current_state[0], window_size)
         states.extend(list(current_state)*window_size)
-        signals.extend(current_signal)
+
+        current_signal = ts_generator(current_state[0], window_size)
+        sig_1.extend(current_signal)
+        correlated_signal = current_signal*0.9 + .03 + np.random.randn(len(current_signal))*0.4
+        sig_2.extend(correlated_signal)
+        uncorrelated_signal = ts_generator((current_state[0]+2)%4, window_size)
+        sig_3.extend(uncorrelated_signal)
+
         pi = transition_matrix[current_state]
+    signals = np.stack([sig_1, sig_2, sig_3])
     # print(states)
-    # plt.plot(signals)
+    # f, axes = plt.subplots(3,1)
+    # for i,ax in enumerate(axes):
+    #     ax.plot(signals[i])
     # plt.show()
     return signals, states
 
@@ -68,16 +86,15 @@ def create_signal(n_features, sig_len, window_size=20):
 def ts_generator(state, window_size):
     time_sampler = ts.TimeSampler(stop_time=window_size)
     sampler = time_sampler.sample_regular_time(num_points=window_size)
-    white_noise = ts.noise.GaussianNoise(std=0.2)
+    white_noise = ts.noise.GaussianNoise(std=0.3)
     if state == 0:
-        sig_type = ts.signals.GaussianProcess(kernel="Periodic", lengthscale=1., mean=0., variance=1., p=5) # Scale is too big
+        sig_type = ts.signals.GaussianProcess(kernel="Periodic", lengthscale=1., mean=0., variance=.1, p=5) # Scale is too big
     elif state == 1:
-        sig_type = ts.signals.GaussianProcess(kernel="SE", lengthscale=1., mean=0., variance=.1)
-    elif state == 2:
-        #sig_type = ts.signals.GaussianProcess(kernel="Linear", c=0.01, offset=-0.5) ## TODO scale appropiately
         sig_type = ts.signals.NARMA(order=5, initial_condition=[0.671, 0.682, 0.675, 0.687, 0.69])
+    elif state == 2:
+        sig_type = ts.signals.GaussianProcess(kernel="SE", lengthscale=1., mean=0., variance=.1)
     elif state == 3:
-        sig_type = ts.signals.NARMA(order=3, initial_condition=[1, 0.97, 0.96])
+        sig_type = ts.signals.NARMA(order=3, coefficients=[0.3, 0.05, 2.5, -0.005], initial_condition=[1, 0.97, 0.96])
 
     timeseries = ts.TimeSeries(sig_type, noise_generator=white_noise)
     samples, _, _ = timeseries.sample(sampler)
@@ -92,15 +109,21 @@ def normalize(train_data, test_data, config='mean_normalized'):
     d = [x.T for x in train_data]
     d = np.stack(d, axis=0)
     if config == 'mean_normalized':
-        feature_means = np.tile(np.mean(d.reshape(-1, feature_size), axis=0), (sig_len, 1)).T
-        feature_std = np.tile(np.std(d.reshape(-1, feature_size), axis=0), (sig_len, 1)).T
+        # feature_means = np.tile(np.mean(d.reshape(-1, feature_size), axis=0), (sig_len, 1)).T
+        feature_means = np.mean(train_data, axis=(0,2))
+        # feature_std = np.tile(np.std(d.reshape(-1, feature_size), axis=0), (sig_len, 1)).T
+        feature_std = np.std(train_data, axis=(0, 2))
         np.seterr(divide='ignore', invalid='ignore')
-        train_data_n = np.array(
-            [np.where(feature_std == 0, (x - feature_means), (x - feature_means) / feature_std) for
-             x in train_data])
-        test_data_n = np.array(
-            [np.where(feature_std == 0, (x - feature_means), (x - feature_means) / feature_std) for
-             x in test_data])
+        train_data_n = train_data - feature_means[np.newaxis,:,np.newaxis]/\
+                       np.where(feature_std == 0, 1, feature_std)[np.newaxis,:,np.newaxis]
+        test_data_n = test_data - feature_means[np.newaxis, :, np.newaxis] / \
+                       np.where(feature_std == 0, 1, feature_std)[np.newaxis, :, np.newaxis]
+        # train_data_n = np.array(
+        #     [np.where(feature_std == 0, (x - feature_means), (x - feature_means) / feature_std) for
+        #      x in train_data])
+        # test_data_n = np.array(
+        #     [np.where(feature_std == 0, (x - feature_means), (x - feature_means) / feature_std) for
+        #      x in test_data])
     elif config == 'zero_to_one':
         feature_max = np.tile(np.max(d.reshape(-1, feature_size), axis=0), (sig_len, 1)).T
         feature_min = np.tile(np.min(d.reshape(-1, feature_size), axis=0), (sig_len, 1)).T
@@ -110,4 +133,4 @@ def normalize(train_data, test_data, config='mean_normalized'):
 
 
 if __name__ == '__main__':
-    main(n_samples=500, n_features=1, sig_len=800)
+    main(n_samples=500, sig_len=800)
